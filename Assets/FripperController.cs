c9da59365cc2f139e25eb7cfaab2a690e8f2c7a1
shobocco <73518173+shobocco@users.noreply.github.com>
2025-11-25 20:12:22 +0900

    Update FripperController.cs
    タップでのフリッパー操作に対応

--- Assets/FripperController.cs
+++ Assets/FripperController.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using System.Collections;
 
 
 public class FripperController : MonoBehaviour
@@ -11,6 +12,9 @@ public class FripperController : MonoBehaviour
     private float defaultAngle = 20;
     //弾いた時の傾き
     private float flickAngle = -20;
+    //タップの状態を保存しておく
+    private int rightTap = -1;
+    private int leftTap = -1;
 
     // Start is called before the first frame update
     void Start ()
@@ -26,26 +30,66 @@ public class FripperController : MonoBehaviour
     void Update ()
     {
 
-        //左矢印キーを押した時左フリッパーを動かす
+        //対象キー(←↓AS)が押された時左フリッパーを動かす
         if ((Keyboard.current.leftArrowKey.wasPressedThisFrame || Keyboard.current.downArrowKey.wasPressedThisFrame || Keyboard.current[Key.A].wasPressedThisFrame || Keyboard.current[Key.S].wasPressedThisFrame) && this.gameObject.CompareTag("LeftFripperTag"))
         {
             SetAngle (this.flickAngle);
         }
-        //右矢印キーを押した時右フリッパーを動かす
+        //対象キー(→↓DS)を押した時右フリッパーを動かす
         if ((Keyboard.current.rightArrowKey.wasPressedThisFrame || Keyboard.current.downArrowKey.wasPressedThisFrame || Keyboard.current[Key.D].wasPressedThisFrame || Keyboard.current[Key.S].wasPressedThisFrame) && this.gameObject.CompareTag("RightFripperTag"))
         {
             SetAngle (this.flickAngle);
         }
 
-        //矢印キー離された時フリッパーを元に戻す
+        //対象キー(←↓AS)を離された時左フリッパーを元に戻す
         if ((Keyboard.current.leftArrowKey.wasReleasedThisFrame || Keyboard.current.downArrowKey.wasReleasedThisFrame || Keyboard.current[Key.A].wasReleasedThisFrame || Keyboard.current[Key.S].wasReleasedThisFrame) && this.gameObject.CompareTag("LeftFripperTag"))
         {
             SetAngle (this.defaultAngle);
         }
+        //対象キー(→↓DS)を離された時右フリッパーを元に戻す
         if ((Keyboard.current.rightArrowKey.wasReleasedThisFrame || Keyboard.current.downArrowKey.wasReleasedThisFrame || Keyboard.current[Key.D].wasReleasedThisFrame || Keyboard.current[Key.S].wasReleasedThisFrame) && this.gameObject.CompareTag("RightFripperTag"))
         {
             SetAngle (this.defaultAngle);
         }
+
+        //タッチを取る
+        //タッチを取ったあと逆側に移動して離した場合はfingerIdで判定して戻す
+        foreach (Touch touch in Input.touches)
+        {
+
+            switch (touch.phase)
+            {
+
+                //タップされた
+                case UnityEngine.TouchPhase.Began:
+                    if(touch.position.x >= 540 && rightTap == -1 && this.gameObject.CompareTag("RightFripperTag"))
+                    {
+                        rightTap = touch.fingerId;
+                        SetAngle (this.flickAngle);                        
+                    }else if (touch.position.x < 540 && leftTap == -1 && this.gameObject.CompareTag("LeftFripperTag"))
+                    {
+                        leftTap = touch.fingerId;
+                        SetAngle (this.flickAngle);                         
+                    }
+                    break;
+
+                //タップが離された
+                case UnityEngine.TouchPhase.Ended:
+                    if(touch.fingerId == rightTap && this.gameObject.CompareTag("RightFripperTag"))
+                    {
+                        rightTap = -1;
+                        SetAngle (this.defaultAngle);                        
+                    }else if (touch.fingerId == leftTap && this.gameObject.CompareTag("LeftFripperTag"))
+                    {
+                        leftTap = -1;
+                        SetAngle (this.defaultAngle);                            
+                    }
+                    break;
+                
+            }
+        }
+
+
     }
 
     //フリッパーの傾きを設定
